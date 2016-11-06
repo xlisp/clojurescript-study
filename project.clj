052b6965b40be390ba1805ded0bad340c1aef2de
chenshunli <chenshunli@ulaiber.com>
2016-11-06 07:55:21 -0800

    3449 -> 3889

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
                            ;; in the default browser once Figwheel has
                            ;; started and complied your application.
                            ;; Comment this out once it no longer serves you.
-                           :open-urls ["http://localhost:3449/index.html"]}
+                           :open-urls ["http://localhost:3889/index.html"]}
 
                 :compiler {:main todos.core
                            :asset-path "js/compiled/out"
@@ -54,7 +54,7 @@
                            :pretty-print false}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
+             :server-port 3889 ;; default
              ;; :server-ip "127.0.0.1"
 
              :css-dirs ["resources/public/css"] ;; watch and update CSS
